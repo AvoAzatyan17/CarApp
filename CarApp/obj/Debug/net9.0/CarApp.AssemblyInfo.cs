3f81b072a97ebde5faa13bf9407ec611b9408bb7
Avetis Azatyan <avetis.azatyan17@gmail.com>
2025-06-05 22:32:10 +0400

    Add softDelete in CarRepository.cs

--- CarApp/obj/Debug/net9.0/CarApp.AssemblyInfo.cs
+++ CarApp/obj/Debug/net9.0/CarApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cf8bb42a91636d5e39c2a2f5e6e5b58bed7ccb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb82bb8969e4bb7b84c772c57998af5380018cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
